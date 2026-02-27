a48289eaa9aed74b6967bd2820f7c08b9d666dbb
abii-63 <a.palanco05@gmail.com>
2026-02-27 19:42:26 +0100

    App vue bien hecho

--- psi-lab2/tutorial-vue/src/App.vue
+++ psi-lab2/tutorial-vue/src/App.vue
@@ -1,4 +1,4 @@
-<!-- App.vue -->
+<!-- -->
 <template>
   <div
     id="app"
@@ -13,8 +13,8 @@
       <div class="col-md-12">
         <formulario-persona @add-persona="agregarPersona" />
         <tabla-personas
-          :personas="personas" 
-          @delete-persona="eliminarPersona" 
+          :personas="personas"
+          @delete-persona="eliminarPersona"
           @actualizar-persona="actualizarPersona"
         />
       </div>
@@ -25,42 +25,41 @@
 <script setup>
 import TablaPersonas from '@/components/TablaPersonas.vue'
 import FormularioPersona from '@/components/FormularioPersona.vue'
-import { ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
+// import { useCounterStore } from '@/stores/counter'; // usar en método correspondiente
 
-// definicion del componente
 defineOptions({
-  // nombre del componente
-  name: 'App',
+  name: 'app',
 });
 
 const personas = ref([]);
 
 const listadoPersonas = async () => {
   // Metodo para obtener un listado de personas
-  try { const response = await fetch('https://my-json-server.typicode.com/ rmarabini/people/personas/');
-  personas.value = await response.json();
-  } catch (error) { console.error(error);}
+  try {
+    const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/');
+    personas.value = await response.json();
+  } catch (error) {
+    console.error(error);
+  }      
 };
 
-const agregarPersona = (persona) => {
+const agregarPersona = async (persona) => {
   try {
-    const response = await fetch('https://my-json-server.typicode.com/ rmarabini/people/personas/', 
-                                  {
-                                    method: 'POST', 
-                                    body: JSON.stringify(persona), 
-                                    headers: { 'Content-type': 'application/json; charset=UTF-8' },
-                                  }
-    );
-
+    const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/', {
+      method: 'POST',
+      body: JSON.stringify(persona),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    });
+    
     const personaCreada = await response.json();
     personas.value = [...personas.value, personaCreada];
-  } catch (error) 
-  {
+  } catch (error) {
     console.error(error);
   }
 };
 
-const eliminarPersona = (id) => {
+const eliminarPersona = async (persona_id) => {
   // Metodo para eliminar una persona
   try {
     await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/'+persona_id+'/', {
@@ -70,34 +69,34 @@ const eliminarPersona = (id) => {
     personas.value= personas.value.filter(u => u.id !== persona_id);
   } catch (error) {
     console.error(error);
-  }
+  }      
 };
 
-const actualizarPersona = (id, personaActualizada) => {
+const actualizarPersona = async (id, personaActualizada) => {
+  // Metodo para actualizar una persona
   try {
-        const response = await fetch('https://my-json-server.typicode. com/rmarabini/people/personas/'+personaActualizada.id+'/', 
-                                      {
-                                        method: 'PUT',
-                                        body: JSON.stringify(personaActualizada),
-                                        headers: { 'Content-type': 'application/json; charset=UTF-8'},
-                                      }
-        );
-        const personaActualizadaJS = await response.json();
-        personas.value = personas.value.map(u => (u.id === personaActualizada.id ? personaActualizadaJS : u));
-  } catch (error) {console.error(error);}
+      const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/'+personaActualizada.id+'/', {
+          method: 'PUT',
+          body: JSON.stringify(personaActualizada),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+          });
+
+      const personaActualizadaJS = await response.json();
+      personas.value = personas.value.map(u => (u.id === personaActualizada.id ? personaActualizadaJS : u));         
+  } catch (error) {
+    console.error(error);
+  }      
 };
 
 // Fetch data when the component is mounted
 onMounted(() => {
   listadoPersonas();
 });
-
 </script>
 
 <style>
-  /* Estilos globales para todos los elementos button en la aplicacion */
   button {
-  background: #009435;
-  border: 1px solid #009435;
+    background: #009435;
+    border: 1px solid #009435;
   }
 </style>
